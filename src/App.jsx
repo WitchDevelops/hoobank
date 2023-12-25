f2dd58e39559241eac97140ed8b52371282d7166
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-25 20:55:23 +0100

    feat: main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Billing, Business, Button, CardDeal, Clients, CTA, FeedbackCard, Footer, GetStarted, Hero, Navbar, Stats, Testimonials } from './components';
+import styles from './style';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <nav className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </nav>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
