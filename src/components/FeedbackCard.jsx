02eda45c0aef5eff28a3121f7bd815bacca4b9ba
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-26 14:06:38 +0100

    feat: testimonials

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { quotes } from '../assets';
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ content, name, title, img }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card font-poppins text-white'>
+      <img src={quotes} alt='quotes' className='w-[42px] h-[27px] object-contain' />
+      <p className=' font-normal text-[18px] leading-[32px] my-10'>
+        {content}
+      </p>
+      <div className='flex flex-row items-center'>
+        <img src={img} alt={name} className='w-[48px] h-[48px] object-contain' />
+        <div className='flex flex-col ml-4'>
+          <h4 className=' font-semibold text-[20px] leading-[32px] '>
+            {name}
+          </h4>
+          <p className=' font-normal text-[16px] leading-[24px] text-dimWhite'>
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
